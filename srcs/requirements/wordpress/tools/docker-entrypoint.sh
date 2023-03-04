4090470feece5986be719d9bc1d245d4f694c4c0
Luis Prates <lprates@student.42lisboa.com>
2023-03-04 17:23:10 +0000

    Inception success

--- srcs/requirements/wordpress/tools/docker-entrypoint.sh
+++ srcs/requirements/wordpress/tools/docker-entrypoint.sh
@@ -1,32 +1,28 @@
 #!/bin/sh
 
-# Sets the script to run in debug mode, which displays commands and their
-# arguments as they are executed
 set -x
-
-#sleep 5
 
 if [ -f /usr/local/bin/.docker-entrypoint-finished ]; then
 	rm -f /usr/local/bin/.docker-entrypoint-finished
 	echo "Removed .docker-entrypoint-finished"
 fi
 
-# Check if config file has already been created by a previous run of this script
+# Checks if the config file has already been created by a previous run of this script
 if [ -e /etc/php/7.3/fpm/pool.d/www.conf ]; then
 	  echo "FastCGI Process Manager config already created"
 else
 
-    # Hydrate configuration template with env variables and create config file
+    # Substitutes env variables and creates config file
     cat /www.conf.tmpl | envsubst > /etc/php/7.3/fpm/pool.d/www.conf
 	chmod 755 /etc/php/7.3/fpm/pool.d/www.conf
 fi
 
-# Check if wp-config.php file has already been created by a previous run
+# Checks if wp-config.php file has already been created by a previous run of this script
 if [ -e wp-config.php ]; then
 	  echo "Wordpress config already created"
 else
 
-    # Create the wordpress config file using environment variables
+    # Create the wordpress config file
     wp config create --allow-root \
         --dbname=$WORDPRESS_DB_NAME \
         --dbuser=$MARIADB_USER \
@@ -41,7 +37,7 @@ if wp core is-installed --allow-root; then
 	  echo "Wordpress core already installed"
 else
 
-    # Install wordpress with environment variables
+    # Installs wordpress
     wp core install --allow-root \
         --url=$WORDPRESS_URL \
         --title=$WORDPRESS_TITLE \
@@ -56,11 +52,11 @@ else
         --role=author \
         --user_pass=$WORDPRESS_USER_PASSWORD
 
-    # Set the debugging mode of WordPress to false which will turn off debugging
-    # messages, error reporting, and logging. Needed when using CLI from container
+    # Turns off debugging which is needed when using CLI from container
     wp config set WORDPRESS_DEBUG false --allow-root
 fi
 
+# Check if author user has already been created by a previous run of this script
 if !(wp user list --field=user_login --allow-root | grep $WORDPRESS_USER_NAME); then
 
 	# create a new author user
@@ -74,6 +70,7 @@ fi
 
 wp plugin update --all --allow-root
 
+# Sets the correct port to listen to nginx
 sed -ie 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 0.0.0.0:9000/g' \
 /etc/php/7.3/fpm/pool.d/www.conf
 
